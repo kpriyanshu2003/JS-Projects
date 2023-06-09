78111c1a7a6a7628c9e8319b08a4d0fa4cbb17a1
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-06-10 01:03:17 +0530

    Updated README and minor programs

--- data/page_url.js
+++
@@ -1,7 +0,0 @@
-document.getElementById(
-  "root"
-).innerHTML = `<button style="margin: 20px" onclick="url();">Get page URL</button><br>
-<div id="result"></div>`;
-function url() {
-  document.getElementById("result").innerText = document.URL;
-}
